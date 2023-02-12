c30ac8960cc417ff4aaead46000c04a8a0d5b796
lujain20 <lujainghaith20@gmail.com>
2023-02-12 15:54:10 +0300

    first commit

---
+++ webapp/controller/Page1.controller.js
@@ -0,0 +1,71 @@
+sap.ui.define(["sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox",
+	"./Dialog1",
+	"./utilities",
+	"sap/ui/core/routing/History"
+], function(BaseController, MessageBox, Dialog1, Utilities, History) {
+	"use strict";
+
+	return BaseController.extend("sap.build.standard.untitledPrototype.controller.Page1", {
+		handleRouteMatched: function(oEvent) {
+			var sAppId = "App63e6613d9c23b70177eafd0a";
+
+			var oParams = {};
+
+			if (oEvent.mParameters.data.context) {
+				this.sContext = oEvent.mParameters.data.context;
+
+			} else {
+				if (this.getOwnerComponent().getComponentData()) {
+					var patternConvert = function(oParam) {
+						if (Object.keys(oParam).length !== 0) {
+							for (var prop in oParam) {
+								if (prop !== "sourcePrototype" && prop.includes("Set")) {
+									return prop + "(" + oParam[prop][0] + ")";
+								}
+							}
+						}
+					};
+
+					this.sContext = patternConvert(this.getOwnerComponent().getComponentData().startupParameters);
+
+				}
+			}
+
+			var oPath;
+
+			if (this.sContext) {
+				oPath = {
+					path: "/" + this.sContext,
+					parameters: oParams
+				};
+				this.getView().bindObject(oPath);
+			}
+
+		},
+		_onButtonPress: function(oEvent) {
+
+			var sDialogName = "Dialog1";
+			this.mDialogs = this.mDialogs || {};
+			var oDialog = this.mDialogs[sDialogName];
+			if (!oDialog) {
+				oDialog = new Dialog1(this.getView());
+				this.mDialogs[sDialogName] = oDialog;
+
+				// For navigation.
+				oDialog.setRouter(this.oRouter);
+			}
+
+			var context = oEvent.getSource().getBindingContext();
+			oDialog._oControl.setBindingContext(context);
+
+			oDialog.open();
+
+		},
+		onInit: function() {
+			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			this.oRouter.getTarget("Page1").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
+
+		}
+	});
+}, /* bExport= */ true);
